250edd8477793598df79eb46f1d1dca515f21ca8
qiang.xue <qiang.xue@5d2665ea-d37e-11dd-9d6a-15002d8776aa>
2008-12-26 20:04:11 +0000

    added yii 1.0.0 and 1.0.1

---
+++ yii-1.0.1/index2.php
@@ -0,0 +1,7 @@
+<?php
+
+// include Yii bootstrap file
+require_once(dirname(__FILE__).'/framework/yiilite.php');
+
+// create a Web application instance and run
+Yii::createWebApplication()->run();
