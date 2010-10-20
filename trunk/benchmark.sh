5f01072d3790d2013532e2571b0f672ab0e58dbc
weizhuo <weizhuo@5d2665ea-d37e-11dd-9d6a-15002d8776aa>
2010-10-20 03:13:57 +0000

    add verify url

--- trunk/benchmark.sh
+++ trunk/benchmark.sh
@@ -3,6 +3,7 @@
 # Change the following settings if needed
 BENCH_INIT="/usr/sbin/ab -n 5";
 BENCH_EXEC="/usr/sbin/ab -t 30 -c 10"
+CURL_EXEC="/usr/bin/curl"
 HTTP_START='sudo /etc/init.d/apache2 start'
 HTTP_STOP='sudo /etc/init.d/apache2 stop'
 
@@ -20,25 +21,45 @@ fi
 
 BASE_URI=$1
 
-while read LINE
-do
-    # Find lines with "framework name: /uri", ignoring lines startings with "#"
-    if echo $LINE |  grep -v "^#" | grep -q ": \+/"
-    then
-        TEST=${LINE%:*}
-        URI=`echo ${LINE#*:} | sed 's/^ *//' | sed 's/ *$//'`
-        TEST_URL="$BASE_URI$URI"
-        echo "Testing $TEST_URL"
+read -p "Verify urls (y/n)? "
+if [ "$REPLY" == "y" ]; then
+    while read LINE
+    do
+        # Find lines with "framework name: /uri", ignoring lines startings with "#"
+        if echo $LINE |  grep -v "^#" | grep -q ": \+/"
+        then
+            TEST=${LINE%:*}
+            URI=`echo ${LINE#*:} | sed 's/^ *//' | sed 's/ *$//'`
+            TEST_URL="$BASE_URI$URI"
+            echo "Verfying $TEST_URL"
+            $CURL_EXEC $TEST_URL
+            echo ""
+            echo "---"
+        fi
+    done < $CONFIG_FILE
+fi
 
-        # Restart apache, warm up the target url, hit it and sleep.
-        $HTTP_STOP
-        $HTTP_START
-        sleep 2
-        $BENCH_INIT $TEST_URL > /dev/null 2>&1
-        echo "~~~ Testing $TEST : $TEST_URL" >> $OUTPUT_FILE
-        $BENCH_EXEC $TEST_URL >> $OUTPUT_FILE
-        sleep 3
-    fi
-done < $CONFIG_FILE
+read -p "Run benchmark (y/n)? "
+if [ "$REPLY" == "y" ]; then
+    while read LINE
+    do
+        # Find lines with "framework name: /uri", ignoring lines startings with "#"
+        if echo $LINE |  grep -v "^#" | grep -q ": \+/"
+        then
+            TEST=${LINE%:*}
+            URI=`echo ${LINE#*:} | sed 's/^ *//' | sed 's/ *$//'`
+            TEST_URL="$BASE_URI$URI"
+            echo "Testing $TEST_URL"
 
-cat $OUTPUT_FILE | grep -e Path -e Request
+            # Restart apache, warm up the target url, hit it and sleep.
+            $HTTP_STOP
+            $HTTP_START
+            sleep 2
+            $BENCH_INIT $TEST_URL > /dev/null 2>&1
+            echo "~~~ Testing $TEST : $TEST_URL" >> $OUTPUT_FILE
+            $BENCH_EXEC $TEST_URL >> $OUTPUT_FILE
+            sleep 3
+        fi
+    done < $CONFIG_FILE
+    cat $OUTPUT_FILE | grep -e Path -e Request
+fi
